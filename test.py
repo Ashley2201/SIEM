56911b2c29562c138fa9b5b2b56a15467564cbd7
Ashley2201 <130235743+Ashley2201@users.noreply.github.com>
2024-03-11 09:06:43 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+import logging
+from scapy.all import sniff
+import psutil
+
+logging.basicConfig(filename='system_network.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def packet_handler(packet):
+
+    logging.info(f"Packet received: {packet.summary()}")
+    if ICMP in packet:
+        icmp_packet = packet[ICMP]
+        if len(icmp_packet) > 65535:
+            logging.warning("Ping of Death detected!")
+
+def log_system_resources():
+    cpu_percent = psutil.cpu_percent(interval=1)
+    memory_percent = psutil.virtual_memory().percent
+    disk_percent = psutil.disk_usage('/').percent
+
+    if(cpu_percent >= 90):
+        logging.warning(f"High CPU Usage: {cpu_percent}%")
+    else:
+        logging.info(f"CPU Usage: {cpu_percent}%")
+    if(memory_percent > 80):
+        logging.warning(f"High Memory Usage: {memory_percent}%")
+    else:
+        logging.info(f"Memory Usage: {memory_percent}%")
+    if(disk_percent < 10):
+        logging.warning(f"Low Disk Space: {100 - disk_percent}%")
+    else:
+        logging.info(f"Disk Usage: {disk_percent}%")
+
+sniff(prn=packet_handler, store=False)
+
+
+while True:
+    log_system_resources()
